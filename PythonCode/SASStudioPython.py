0028047ca6a71df33540365e88fa470455f13a45
taorthe@gmail.com <taorthe@gmail.com>
2021-12-02 09:08:06 +0000

    Initital versions

---
+++ PythonCode/SASStudioPython.py
@@ -0,0 +1,17 @@
+import os, sys
+sys.path
+
+input_table = 'SASHELP.CLASS'
+output_table = 'WORK.PYTHONOUT'
+
+dfin = SAS.sd2df(input_table)
+
+print("input data shape is:", dfin.shape)
+
+dfout = dfin.transpose()
+
+print("output data shape is:", dfout.shape)
+
+SAS.df2sd(dfout, output_table)
+
+SAS.submit('proc print  data=work.pythonout;run;')
